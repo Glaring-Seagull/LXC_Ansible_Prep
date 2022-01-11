32845730ff5a5e6e7e1ec536b544374d72cbe8c9
Glaring-Seagull <76177943+Glaring-Seagull@users.noreply.github.com>
2022-01-11 15:03:02 -0500

    Create LXC_Ansbile_Prep.sh

---
+++ LXC_Ansbile_Prep.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#This script is for prepping LXC containers for SSH and Ansible use
+
+#checks for yum or apt then uses the detected package manager to install ssh and sudo; exits if neither is detected
+if command -v apt-get >/dev/null; then
+  apt install openssh-server sudo -y
+elif command -v yum >/dev/null; then
+  yum install openssh-server sudo -y
+else
+  echo "Not able to detect package manager! Canceling script!"
+  exit 1
+fi
+
+#Configures permission to be accessed via SSH
+echo "PermitRootLogin yes" >> /etc/ssh/ssh_config
+
+#Enable ssh so it starts every time and restarts service to complete changes
+systemctl enable sshd.service
+systemctl restart sshd.service

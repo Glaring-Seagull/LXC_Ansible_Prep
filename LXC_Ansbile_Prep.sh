0a9425c3e40155ab62345ac06ea30a45ed582ba0
Glaring-Seagull <76177943+Glaring-Seagull@users.noreply.github.com>
2022-06-30 09:13:08 -0400

    Update LXC_Ansbile_Prep.sh

--- LXC_Ansbile_Prep.sh
+++ LXC_Ansbile_Prep.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#This script is for prepping LXC containers for SSH and Ansible use
+#This script is for prepping LXC containers for SSH and Ansible use, inteded for systemd based systems
 
 #checks for yum or apt then uses the detected package manager to install ssh and sudo; exits if neither is detected
 if command -v apt-get >/dev/null; then
   apt install openssh-server sudo -y
-elif command -v yum >/dev/null; then
-  yum install openssh-server sudo -y
+elif command -v dnf >/dev/null; then
+  dnf install openssh-server sudo -y
 else
   echo "Not able to detect package manager! Canceling script!"
   exit 1
